beeecadd6ee868c1d26f5eba3bd88e40bcddab16
mikishlep <gleb.tseilonsky@yandex.ru>
2024-08-04 06:58:52 +0500

    rot ibal

--- frontend/src/homepage/Dashboard.jsx
+++ frontend/src/homepage/Dashboard.jsx
@@ -3,6 +3,7 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill } from 'react-icons/bs';
 import { useSpring, animated } from "react-spring";
 import { FaPlus, FaTrash, FaTimes } from 'react-icons/fa';
+import axios from 'axios';
 
 function Number({ n }) {
     const { number } = useSpring({
@@ -16,49 +17,6 @@ function Number({ n }) {
 };
 
 function Dashboard() {
-    const [moneyData, setMoneyData] = useState([
-        { name: 'Январь', расходы: 0, доходы: 0, amt: 2400 },
-        { name: 'Февраль', расходы: 0, доходы: 0, amt: 2210 },
-        { name: 'Март', расходы: 0, доходы: 0, amt: 2290 },
-        { name: 'Апрель', расходы: 0, доходы: 0, amt: 2000 },
-        { name: 'Май', расходы: 0, доходы: 0, amt: 2181 },
-        { name: 'Июнь', расходы: 0, доходы: 0, amt: 2500 },
-        { name: 'Июль', расходы: 0, доходы: 0, amt: 2100 },
-        { name: 'Август', расходы: 0, доходы: 0, amt: 2100 },
-        { name: 'Сентябрь', расходы: 0, доходы: 0, amt: 2100 },
-        { name: 'Октябрь', расходы: 0, доходы: 0, amt: 2100 },
-        { name: 'Ноябрь', расходы: 0, доходы: 0, amt: 2100 },
-        { name: 'Декабрь', расходы: 0, доходы: 0, amt: 2100 }
-    ]);
-
-    const [profitData, setProfitData] = useState([
-        { name: 'Январь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2400 },
-        { name: 'Февраль', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2210 },
-        { name: 'Март', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2290 },
-        { name: 'Апрель', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2000 },
-        { name: 'Май', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2181 },
-        { name: 'Июнь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2500 },
-        { name: 'Июль', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 },
-        { name: 'Август', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 },
-        { name: 'Сентябрь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 },
-        { name: 'Октябрь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 },
-        { name: 'Ноябрь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 },
-        { name: 'Декабрь', чистая_прибыль: 0, валовая_прибыль: 0, amt: 2100 }
-    ]);
-
-    const [editing, setEditing] = useState(false); // Состояние для показа/скрытия формы ввода данных
-
-    const handleChangeMoneyData = (index, field, value) => {
-        const updatedData = [...moneyData];
-        updatedData[index] = { ...updatedData[index], [field]: +value };
-        setMoneyData(updatedData);
-    };
-
-    const handleChangeProfitData = (index, field, value) => {
-        const updatedData = [...profitData];
-        updatedData[index] = { ...updatedData[index], [field]: +value };
-        setProfitData(updatedData);
-    };
 
     const [cardData, setCardData] = useState([
         { title: 'ПАМЯТНИКИ', count: 1 },
@@ -66,12 +24,6 @@ function Dashboard() {
         { title: 'СТОЛЕШНИЦЫ', count: 0 },
         { title: 'РАКОВИНЫ', count: 0 },
     ]);
-
-    const handleChangeCardData = (index, field, value) => {
-        const updatedData = [...cardData];
-        updatedData[index] = { ...updatedData[index], [field]: +value };
-        setCardData(updatedData);
-    };
 
     return (
         <section id='dashboard-main' className='main-container'>
